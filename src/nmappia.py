4067dbcb46dd89c22e5c900cbd2ef990bd7c1ce6
Matt Franz <mdfranz@gmail.com>
2015-11-08 19:56:21 -0500

    Add python-libnmap

--- src/nmappia.py
+++ src/nmappia.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python
 
-DEBUG=True
+import json,syslog,time
 
-import json
+from libnmap.parser import NmapParser
+from libnmap.process import NmapProcess
+
 
 class ConfigFile(object):
-    def __init__(self,f="nmappia.conf"):
-        self.debug = True
-        self.tmpdir = "/tmp"
+    def __init__(self,f="nmappia.conf",debug=True):
+        self.targets = []
+        self.debug = debug
+        self.options = ""
         with open(f) as config_file:
             self.config = json.load(config_file)
 
             if self.debug:
+                print "=== Configuration ==="
                 print `self.config`
 
-    def make_command(self):
-        """Form nmap command string"""
-        command = " %s " % (self.config['policy']['scan_options'])
+    def generate_targets(self):
+        syslog.syslog("Starting target generation")
 
+        if self.config['inputs'].has_key('networks'):
+            for n in self.config['inputs']['networks']:
+                self.targets.append(n)
+            
+        if self.debug:
+            print "=== Targets ==="
+            print `self.targets`
+        
+        syslog.syslog("Completed target generation")
+
+    def form_command(self):
+        """Form nmap command string"""
+        self.options = " %s " % (self.config['policy']['scan_options'])
+
+class ScanEngine(object):
+    def __init__(self,config):
+        self.engine = NmapProcess(config.targets, config.options)
+
+    def run(self):
+        """Execute scan"""
+        syslog.syslog("Starting scan")
+        self.engine.run_background()
+        while self.engine.is_running():
+            time.sleep(30)
+            print "Scan is %s percent complete" % self.engine.progress
+        
 if __name__ == "__main__":
     c = ConfigFile()
+    c.generate_targets()
+    se = ScanEngine(c)
+    se.run()
